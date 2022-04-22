655ddc431baa46a94a4a68984581b7e8ac16904f
Nataly <bakhmutskaya@mail.ru>
2022-04-22 20:51:37 +0300

    new2

---
+++ Lesson (Тренировка).cpp
@@ -0,0 +1,13 @@
+﻿// Lesson (Тренировка).cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "const.h"
+
+using namespace std;
+
+int main()
+{
+    cout << pi << endl;
+}
+
